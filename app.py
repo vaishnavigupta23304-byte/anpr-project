71ca4069caeee2e7d4ad8854780c5267002db9df
Vaishnavi Gupta <vaishanvi@gmail.com>
2026-06-30 23:51:28 +0530

    Fix EasyOCR dependencies

--- app.py
+++ app.py
@@ -210,7 +210,6 @@ def vehicles():
     ])
 
 
-# ⭐ PRODUCTION START
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
